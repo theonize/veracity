dabedcb0bb8e597b38508e28dd624ac61ade5c0b
Matthew Currier <matthew.currier@mhwirth.com>
2014-11-13 10:55:06 -0600

    search highlighting complete

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
 $(function() {
-	$('li:has(~ol)').click(function() {
+	var search = $('#search');
+
+	// this selects all but the leaf-nodes of the nested lists
+	// nfo: the outer list is a ul, inner lists are ol
+	var allListItems = $('li')
+
+	var innerListItems = allListItems.filter(':has(~ol)');
+
+	highlightLines();
+
+	innerListItems.click(function() {
 		$(this).next().toggle(100, function() {
 			highlightLines();
 		});
 	});
 
-	highlightLines();
+	search.change(function() {
+		// when a search term is entered highlight the word and it's parent and grandparent
+		var searchTerm = search.val();
 
-	// when a search term is entered highlight the word and it's parent and grandparent
+		var query = ":contains('" + searchTerm + "')";
+
+		var items = allListItems.filter(":contains('" + searchTerm + "')");
+
+		allListItems.removeClass('search');
+
+		items
+		.addClass('search');
+
+		items
+		.parent()
+		.prev()
+		.addClass('search');
+
+		items
+		.parent()
+		.parent()
+		.prev()
+		.addClass('search');
+	});
 });
 
 function highlightLines() {
